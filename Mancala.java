74db1dfd22bde4f2bd2e512a2d33da6d12cc9ebb
iyi1305 <155403724+iyi1305@users.noreply.github.com>
2024-01-02 18:04:05 -0800

    fixing errors

--- Mancala.java
+++ Mancala.java
@@ -1,20 +1,40 @@
-public class Mancala {
-    private int stoneCount;
-    
-    public Mancala() {
-       stoneCount = 0;
-    }
-    
-    public void incrementStones(int stonesToAdd) {
-       stoneCount += stonesToAdd;
-    }
-    
-    public int countStones() {
-       return stoneCount;
-    }
-    
-    public void adjustStones(int newStoneCount) {
-       stoneCount = newStoneCount;
-    }
- }
- 
+
+public class Mancala
+{
+   private int stoneAmount;
+   
+   /**
+    * Constructor for Mancala.
+    */
+   public Mancala()
+   {
+      stoneAmount = 0;
+   }
+   
+   /**
+    * Add stones to this mancala.
+    * @param amount amount of stones to add
+    */
+   public void addStones(int amount)
+   {
+      stoneAmount += amount;
+   }
+   
+   /**
+    * Get stone count from mancala.
+    * @return the amount of stones in the mancala
+    */
+   public int getAmount()
+   {
+      return stoneAmount;
+   }
+   
+   /**
+    * Set the stones in the mancala to a specific number.
+    * @param stone number to set the stones to
+    */
+   public void setStones(int stone)
+   {
+	   stoneAmount = stone;
+   }
+}
